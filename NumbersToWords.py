7ee7b26bebab4bc4cc9fe01fb652684c640bb72e
Steven Bourn <stevenbourn2@hotmail.com>
2021-06-08 09:43:12 +0100

    NumberToWords.py extended to thousands

--- NumbersToWords.py
+++ NumbersToWords.py
@@ -3,17 +3,22 @@ def numWord(n):
     units =["","one","two","three","four","five","six","seven","eight","nine"]
     teens =["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
     tens = ["","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+
+    hundInd = n//100 - 10*(n//1000)
+    tenInd = n//10 - 10*(n//100)
+    unitInd = n - 10*(n//10)
     if len(str(n)) == 1:
         if n == 0:
             return "zero"
         else:
             return units[n]
     elif len(str(n)) ==2:
-        tenInd = n//10
-        unitInd = n - 10*(n//10)
         if n < 20:
             return teens[unitInd]
         return f"{tens[tenInd]} {units[unitInd]}"
+    elif len(str(n)) ==3:
+        return f"{units[hundInd]} hundred and {numWord(tenInd*10+unitInd)}"
+        
 
-for number in range(100):
+for number in range(100,1000):
     print(numWord(number))
