e7efa6abfc5b07455f682d701436b3c4be77d49c
Steven Bourn <stevenbourn2@hotmail.com>
2021-06-08 10:58:15 +0100

    NumbersToWords.py now functional

--- NumbersToWords.py
+++ NumbersToWords.py
@@ -1,24 +1,40 @@
 def numWord(n):
-    #orders_of_magnitude = ["","hundred","thousand","million","billion","trillion"]
+    magnitude = ["thousand","million","billion","trillion"]
     units =["","one","two","three","four","five","six","seven","eight","nine"]
     teens =["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
     tens = ["","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
 
+    numStr = str(n)
     hundInd = n//100 - 10*(n//1000)
     tenInd = n//10 - 10*(n//100)
     unitInd = n - 10*(n//10)
-    if len(str(n)) == 1:
+    if len(numStr) == 1:
         if n == 0:
             return "zero"
         else:
             return units[n]
-    elif len(str(n)) ==2:
+    elif len(numStr) ==2:
         if n < 20:
             return teens[unitInd]
+        if unitInd == 0:
+            return f"{tens[tenInd]}"
         return f"{tens[tenInd]} {units[unitInd]}"
-    elif len(str(n)) ==3:
+    elif len(numStr) ==3:
         return f"{units[hundInd]} hundred and {numWord(tenInd*10+unitInd)}"
-        
+    elif len(numStr) >=15:
+        return "number out of range"
+    elif len(numStr) >3:
+        print("Hello")
+        if len(numStr)%3 == 0:
+            firstString = numStr[:3]
+            secondString = numStr[3:]
+        else:
+            firstString = numStr[:len(numStr)%3]
+            print(firstString)
+            secondString = numStr[len(numStr)%3:]
+        firstInt = int(firstString)
+        secondInt = int(secondString)
+        return f"{numWord(firstInt)} {magnitude[(len(numStr)//3)-1]}, {numWord(secondInt)}"
 
-for number in range(100,1000):
-    print(numWord(number))
+
+print(numWord(2249905467))
