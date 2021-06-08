213c7701379690d1711e8aa7fc0e0e1ec55ebc68
Steven Bourn <stevenbourn2@hotmail.com>
2021-06-08 09:33:52 +0100

    NumbersToWords now handles irregular teens

--- NumbersToWords.py
+++ NumbersToWords.py
@@ -1,7 +1,7 @@
 def numWord(n):
     #orders_of_magnitude = ["","hundred","thousand","million","billion","trillion"]
     units =["","one","two","three","four","five","six","seven","eight","nine"]
-    #teens =["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+    teens =["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
     tens = ["","ten","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
     if len(str(n)) == 1:
         if n == 0:
@@ -9,7 +9,11 @@ def numWord(n):
         else:
             return units[n]
     elif len(str(n)) ==2:
-        return f"{tens[n//10]} {units[n-10*(n//10)]}"
+        tenInd = n//10
+        unitInd = n - 10*(n//10)
+        if n < 20:
+            return teens[unitInd]
+        return f"{tens[tenInd]} {units[unitInd]}"
 
 for number in range(100):
     print(numWord(number))
