fb23a3c74a31bbe76d7eedf204abf82e7040db1f
Steven Bourn <stevenbourn2@hotmail.com>
2021-06-10 17:07:37 +0100

    debug examples

---
+++ programs/debug_excer4.py
@@ -0,0 +1,6 @@
+
+item_list = ["Burger", "Hotdog", "Bun", "Ketchup", "Cheese"]
+for i in item_list:
+    print(i)
+
+print(item_list[4])
---
+++ programs/debug_tut.py
@@ -0,0 +1,13 @@
+
+def final(var1, var2, var3):
+    breakpoint()
+    return var1+var2+var3
+
+breakpoint()
+
+a = "aaa"
+b = "bbb"
+c = "ccc"
+
+final = final(a, b, c)
+print(final)
